ba9e31388906003b57512cbb59fc6b8b0a5f20f9
Nulan Musayev <nurlan_plaz@mil.ru>
2023-09-16 16:39:43 +0400

    GoldXp

---
+++ week2/Day5/GoldXp/script.js
@@ -0,0 +1,40 @@
+let player1 = prompt("please add a word");
+let arrOfPlayer1 = player1.split("");
+let regpassword = player1.match(/\b\w{8,}\b/g);
+let player1Consol = "";
+let arrOfPlayer1Consol;
+let chance = 10;
+if (regpassword) {
+    // alert("welldone")
+    password(player1);
+    for (let y = 0; y < player1.length; y++) {
+        let player2 = prompt("please add a letter");
+        let regex2 = player2.match(/^[A-Za-z]+$/)
+        if (regex2 && player2.length < 2) {
+            arrOfPlayer1Consol = player1Consol.split("");
+            if (player1[y] == player2) {
+                arrOfPlayer1Consol[y] = player2;
+                player1Consol = arrOfPlayer1Consol.join("");
+                console.log(player1Consol);
+            } else {
+
+                chance--;
+                alert(`you had ${chance} more chance`);
+                y--
+            }
+
+
+
+        }
+    }
+} else {
+    alert("the word must be minimum 8 letter please try again .You can use only letter")
+}
+
+function password(a) {
+
+    for (let i = 0; i < a.length; i++) {
+        player1Consol += '*';
+    }
+    return console.log(player1Consol)
+}
