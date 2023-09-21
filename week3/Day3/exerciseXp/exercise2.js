49b06efba1e467b42573f185d00d618acc072a74
nergız seyidova <nergisseyidova@gmail.com>
2023-09-21 11:07:33 +0400

    checker comments

--- week3/Day3/exerciseXp/exercise2.js
+++ week3/Day3/exerciseXp/exercise2.js
@@ -1,12 +1,12 @@
+let pos = 0;
 function myMove() {
-
-    let pos = 0;
-    let animateEl = document.querySelector("#animate");
-    let setTime = setInterval(() => {
-        pos++;
-        animateEl.style.left = pos + "px";
-        if (pos == 350) {
-            clearInterval(setTime)
-        }
-    }, 5)
+  let animateEl = document.querySelector('#animate');
+  let setTime = setInterval(() => {
+    pos++;
+    animateEl.style.left = pos + 'px';
+    if (pos == 350) {
+      clearInterval(setTime);
+      pos = 0;
+    }
+  }, 5);
 }
--- week3/Day3/exerciseXp/exercise3.js
+++ week3/Day3/exerciseXp/exercise3.js
@@ -16,3 +16,19 @@ function takeFunc(e) {
     e.dataTransfer.setData("text", e.target.id);
 
 }
+
+// !!! yazilan kodda qarisiqliq var islemir
+// !!! asagidaki kod isleyir 
+
+/*   ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+} */
