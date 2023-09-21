861bc1e6c6bca5a8ef269764e322130fdd8dabdc
Nulan Musayev <nurlan_plaz@mil.ru>
2023-09-21 16:32:23 +0400

    Day4

--- week3/Day4/ColoringGame/script.js
+++ week3/Day4/ColoringGame/script.js
@@ -1,9 +1,9 @@
 // colors
-let bool = true;
+let bool = false;
 let leftSideCol = document.querySelector(".colors");
 let colors = ["red", "orange", "orangered", "yellow", "lightgreen", "yellowgreen", "darkgreen", "blue", "aqua", "blueviolet", "pink", "black", "lightcoral", "lightcyan", "lightgray", "lightpink", "lightsalmon", "darkblue", "darkcyan", "green", "white"];
 for (let i = 0; i < 21; i++) {
-    let cubicadd = `<div class="cubic col-4 "></div>`
+    let cubicadd = `<input class="cubic col-4 "></input>`
     leftSideCol.innerHTML += cubicadd;
 
 
@@ -11,52 +11,61 @@ for (let i = 0; i < 21; i++) {
 let cubic = document.querySelectorAll(".cubic");
 for (let i = 0; i < cubic.length; i++) {
     cubic[i].style.backgroundColor = colors[i];
-    cubic[i].classList.add(colors[i])
+    cubic[i].value = colors[i]
 }
 let rightZone = document.querySelector(".right-sideBar");
-for (let i = 0; i < 1794; i++) {
+for (let i = 0; i < 1800; i++) {
     let colorZone = `<div class="colorZone"></div>`;
     rightZone.innerHTML += colorZone;
 
 }
+
 ///backside
+
+let pickedColor = " ";
 for (let i = 0; i < colors.length; i++) {
 
     cubic[i].addEventListener("click", function clickColor(e) {
         console.log("i clicked the color");
-        let target = e.target;
-
-        let colorPicked = document.querySelectorAll(".colorZone");
-
-        for (let j = 0; j < colorPicked.length; j++) {
-            colorPicked[j].addEventListener("mousedown", function mDown(e) {
-
-                console.log("i mousedown the zone");
-                e.target.style.backgroundColor = colors[i]
-                if (bool) {
-                    for (let j = 0; j < colorPicked.length; j++) {
+        pickedColor = e.target.value;
 
 
-                        colorPicked[j].addEventListener("mouseover", (e) => {
-                            console.log("i mouseOver on the zone");
-                            e.target.style.backgroundColor = colors[i]
-                            colorPicked[j].addEventListener("mouseup", (e) => {
-                                console.log("mouse upped");
-                                bool = false;
-
-
-
-
-                            })
-
-                        })
-
-                    }
-                }
-            })
-
-
-
-        }
     })
 }
+
+let allDivs = document.querySelectorAll(".colorZone");
+
+for (let div of allDivs) {
+    div.addEventListener("mousedown", function mDown(e) {
+
+        console.log("i mousedown the zone");
+        e.target.style.backgroundColor = pickedColor;
+        bool = true;
+
+
+    })
+    div.addEventListener("mouseup", () => {
+        console.log("i mouseup the zone");
+        bool = false;
+    })
+
+    div.addEventListener("mouseover", (e) => {
+        if (!bool) {
+            return
+        }
+        e.target.style.backgroundColor = pickedColor;
+    })
+
+
+
+
+
+
+}
+
+let button = document.querySelector(".btn");
+button.addEventListener("click", () => {
+    for (let div of allDivs) {
+        div.style.backgroundColor = "white";
+    }
+})
