5c2d285c13d781b808b74d7e501cd6b85a5703c2
nergız seyidova <nergisseyidova@gmail.com>
2023-10-11 11:00:27 +0400

    checker comments

--- week6/Day2/EXXP/Ex1.sql
+++ week6/Day2/EXXP/Ex1.sql
@@ -18,7 +18,7 @@
 -- The first 3 customers in alphabetical order of the first name (A-Z) – exclude the primary key column from the results.
 -- All last names (no other columns!), in reverse alphabetical order (Z-A)
 select * from items ORDER BY item_price ASC ;
-select item_name , item_price from items WHERE item_price>=80 ORDER BY item_price ASC ;
+select item_name , item_price from items WHERE item_price>=80 ORDER BY item_price ASC ; /* highest to lowest DESC */
 SELECT customer_name , customer_lastname FROM customers ORDER BY customer_name ASC LIMIT 3 
 SELECT customer_lastname FROM customers ORDER BY customer_lastname DESC
 
--- week6/Day2/EXXP/Ex2.sql
+++ week6/Day2/EXXP/Ex2.sql
@@ -18,7 +18,7 @@ select DISTINCT create_date from customer
 SELECT first_name ,last_name , email , address_id  FROM customer ORDER BY first_name DESC
 SELECT film_id , title, description , release_year , rental_rate FROM film ORDER BY rental_rate ASC
 select address , phone from address WHERE district = 'Texas'
-SELECT * FROM film where film_id BETWEEN 15 AND 150
+SELECT * FROM film where film_id BETWEEN 15 AND 150 /* either 15 or 150 --OR */
 SELECT film_id , title, description , release_year , rental_rate FROM film WHERE title like 'In%'
 SELECT * FROM film  ORDER BY rental_rate limit 10 offset 10
 select * from customer
@@ -36,7 +36,7 @@ ON payment.customer_id = customer.customer_id order by customer.customer_id ASC
 
 
 
-select film_id , inventory_id  FROM inventory where film_id ='null'
+select film_id , inventory_id  FROM inventory where film_id ='null' /* IS NULL */
 select title FROM film where film_id NOT IN(SELECT film_id FROM inventory)
 
 
