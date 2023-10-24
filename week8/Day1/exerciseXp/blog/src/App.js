664de1ca1d904eef064205fb568b52b35022a4c2
Nulan Musayev <nurlan_plaz@mil.ru>
2023-10-24 07:13:55 +0400

    first React

---
+++ week8/Day1/exerciseXp/blog/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+//import NewFavoriteAnimals from './UserFavoriteAnimals';
+import Exercise from './exercise3';
+/*const sum = 5+5
+class Car extends React.Component {
+    render() {
+        return <h1>React is {sum} times better with JSX</h1>;
+    }
+}*/
+
+//const user = {
+//  firstName: 'Bob',
+//  lastName: 'Dylan',
+//  favAnimals : ['Horse','Turtle','Elephant','Monkey']
+//};
+////In the App.js file, render the first name and the last name of the user in two <h3>.
+//class FullName extends React.Component{
+//	render(){
+//	return(<div >
+//		<NewFavoriteAnimals animals={user.favAnimals}/>
+//<h3>{user.firstName}</h3>
+//<h3>{user.lastName}</h3>
+//</div>)}
+//}
+
+//export default FullName;
---
+++ week8/Day1/exerciseXp/blog/src/UserFavoriteAnimals.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class NewFavoriteAnimals extends React.Component{
+
+	render(){
+		console.log(this.props.animals);
+		return("inefn"
+			/*user.favAnimals.map(animal=>{
+			<ul key={user.favAnimals}>
+				<li>animal</li>
+			</ul>*/
+	//	}))
+)	}
+}
+
+export default NewFavoriteAnimals
