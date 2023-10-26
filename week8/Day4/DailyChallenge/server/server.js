e80e2910a6afa5d745f39b19b5241a028bf069c4
Nulan Musayev <nurlan_plaz@mil.ru>
2023-10-27 00:15:40 +0400

    dailychallenge

---
+++ week8/Day4/DailyChallenge/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require("cors");
+const app = express();
+const bp = require("body-parser")
+app.use(cors());
+app.use(bp.urlencoded())
+app.use(bp.json())
+app.get("/api/hello", (req, res) => {
+    res.send("Hello From express")
+
+})
+
+app.post("/api/world", (req, res) => {
+
+    res.send({
+        message: `i recived you message: ${req.body.message}`
+    });
+})
+
+app.listen(5002, () => {
+    console.log("server is running");
+})
