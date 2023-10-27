916b415e73cae652627ebd2582bfdb2ade656bf7
Nulan Musayev <nurlan_plaz@mil.ru>
2023-10-27 11:38:58 +0400

    dc2

---
+++ week8/Day4/dailychallenge2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import AutoCompletedText from './components/autosearch';
+
+function App() {
+  return (
+    <div className="App">
+      <AutoCompletedText/>
+    </div>
+  );
+}
+
+export default App;
---
+++ week8/Day4/dailychallenge2/src/components/autosearch.js
@@ -0,0 +1,65 @@
+import countries from "./countries";
+import React, {Component} from "react";
+
+
+
+class AutoCompletedText extends Component{
+
+	constructor(){
+		super();
+		this.state = {
+			text: '',
+			result:[]
+		}
+	}
+
+	handleChange(e){
+		let res = countries.filter((item)=>{
+				let lowCase =	item.toLowerCase();
+				let lowCaseTarget = e.target.value.toLowerCase();
+				return	lowCase.startsWith(lowCaseTarget)
+		});
+
+		this.setState({
+        result: res,
+				text:e.target.value
+    });
+
+		let listStyle = document.querySelector(".listOfCountries").style;
+
+		if (e.target.value==="") {
+				listStyle.display = "none"
+		}else{
+			listStyle.display="flex"
+		}
+
+	}
+
+
+
+
+	render(){
+
+
+
+		return(
+			<div className="card">
+				<h1>Auto Completed</h1>
+				<form>
+					<input type="text" placeholder="add country name" id="countrytInp" value={this.state.text} onChange={this.handleChange.bind(this)}/>
+				</form>
+					<ul className="listOfCountries"  >
+					{
+							this.state.result.map((item)=>{
+							return	<li>{item}</li>
+							})
+							}
+					</ul>
+					<button>Suggestion:{this.state.result.length}</button>
+			</div>
+		)
+
+	}
+}
+
+export default AutoCompletedText
