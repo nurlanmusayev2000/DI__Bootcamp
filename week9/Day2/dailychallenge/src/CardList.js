1ba05ae4569671f89444e981c59c34a869b9e0e8
Nulan Musayev <nurlan_plaz@mil.ru>
2023-11-02 12:15:13 +0400

    dc

--- week9/Day2/dailychallenge/src/CardList.js
+++ week9/Day2/dailychallenge/src/CardList.js
@@ -1,19 +1,34 @@
 import React from "react";
 import Card from './Card'
+import { connect } from "react-redux";
+const CardList =(props)=>{
+console.log("props:",props);
+		let cardComponent ;
+		if (props.searchingRobots.length ===0) {
+			 cardComponent = props.robots.map((user ,index)=>{
+				return(
+					<Card key={user.id} id={index} name={user.name} email={user.email}/>
+				)
+			})
+		}else{
+			cardComponent = props.searchingRobots.map((user ,index)=>{
+				return(
+					<Card key={user.id} id={index} name={user.name} email={user.email}/>
+				)
+			})
+		}
 
-const CardList =({robots})=>{
-
-	const cardComponent = robots.map((user ,index)=>{
-		return(
-			<Card key={user.id} id={robots[index].id} name={robots[index].name} email={robots[index].email}/>
-
-		)
-	})
 	return(
 		<div>
 				{cardComponent}
 		</div>
 	)
 }
-
-export default CardList
+const mapStateToProps = state=>{
+  return{
+    searchField: state.searchField,
+    robots: state.robots,
+		searchingRobots:state.searchingRobots
+  }
+}
+export default connect(mapStateToProps)(CardList);
--- week9/Day2/dailychallenge/src/SearchBox.js
+++ week9/Day2/dailychallenge/src/SearchBox.js
@@ -1,11 +1,36 @@
 import React from "react";
+import {connect} from "react-redux";
+import './App.css';
+import { setSearchField ,checkRobotsAction } from './action';
 
-const SearchBox = ({searchChange})=>{
-	return(
+
+
+
+const SearchBox = (props)=>{
+	console.log("props",props);
+	return(<>
 		<div className="pa2">
-			<input onChange={searchChange} className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
+			<input onChange={props.onSearchChange} className="pa3 ba b--green bg-lightest-blue" type="search" placeholder="search robots" />
 		</div>
+		</>
 	)
 }
 
-export default SearchBox
+
+
+
+
+const mapStateToProps = state=>{
+  console.log(state);
+  return{
+    searchField: state.searchField,
+    robots: state.robots,
+		searchingRobots:state.searchingRobots
+  }
+}
+const mapDispatchToProps=(dispatch)=>{
+  return {
+		onSearchChange: (e)=>dispatch(setSearchField(e.target.value)),
+	}
+}
+export default connect(mapStateToProps,mapDispatchToProps)(SearchBox)
