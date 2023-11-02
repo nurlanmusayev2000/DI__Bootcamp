a19abbd77b854bd8136ed4389d7b8def578cc695
Nulan Musayev <nurlan_plaz@mil.ru>
2023-11-02 12:50:16 +0400

    day1

--- week9/Day1/inclass/src/components/Goodsitem.js
+++ week9/Day1/inclass/src/components/Goodsitem.js
@@ -2,17 +2,11 @@ import React, { Component } from "react";
 import "../App.css";
 import store from "../redux/store";
 import reducer from "../redux/reducer";
+import {connect} from "react-redux";
 
 class GoodsItem extends Component {
 
-addToCardHandler=(id)=>{
-	store.dispatch({
-		type:'ADD_GOOD_TO_CART',
-		payload:{
-			id:id
-		}
-	})
-}
+
 
   render() {
     const { title, description, price, id } = this.props;
@@ -25,10 +19,15 @@ addToCardHandler=(id)=>{
           <span className="goods-item__price-value goods-item__price-value_new">{price}.00$</span>
         </p>
         <p className="goods-item__description">{description}</p>
-        <button className="goods-item__add-to-card" onClick={()=>this.addToCardHandler( id)}>Add to cart</button>
+        <button className="goods-item__add-to-card" onClick={()=>this.props.addGoodToCart( id)}>Add to cart</button>
       </div>
     );
   }
 }
 
-export default GoodsItem;
+const mapDispatchToProps=dispatch=>{
+  return { addGoodToCart: (id)=>{dispatch({type:"ADD_GOOD_TO_CART" , payload:{id:id}})}}
+}
+
+
+export default connect(null , mapDispatchToProps)(GoodsItem)
